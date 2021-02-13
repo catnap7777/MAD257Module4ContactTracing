8afab3e72d5c8fae2c540cb954f9db645b822072
km <35552602+catnap7777@users.noreply.github.com>
2021-02-13 13:18:43 -0600

    Initial Commit

---
+++ MAD257Module4ContactTracing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MAD257Module4ContactTracing
+//
+//  Created by Karen Mathes on 2/13/21.
+//  Copyright © 2021 TygerMatrix Software. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
